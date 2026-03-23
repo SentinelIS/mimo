7c7f17ec21cae7b245c9d480ef0e97f714714a3c
Nathan Dion Schmid <nathandion.schmid@gmail.com>
2026-03-23 09:47:41 +0100

    added /api-docs endpoint

--- dto/ask-question.dto.ts
+++ dto/ask-question.dto.ts
@@ -1,7 +1,8 @@
-
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AskQuestionDto {
+  @ApiProperty({ description: 'The question to ask the chatbot.' })
   @IsString()
   @IsNotEmpty()
   question: string;
